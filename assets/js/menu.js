2781c6f96ca90433cde94087c58d4abd4ac71645
Gustavo Eizo Takahashi <130497619+Takahacker@users.noreply.github.com>
2023-12-05 15:20:48 -0300

    Menu que desliza de cima para baixo ao ser clicado utilizando java script

---
+++ assets/js/menu.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', function() {
+ 
+  const menuIcon = document.querySelector('.hamb');
+
+
+  const mainContent = document.querySelector('main');
+
+  const menuLinks = document.querySelectorAll('.menu-link');
+
+ 
+  menuLinks.forEach(function(link) {
+    link.addEventListener('click', (event) => {
+      event.preventDefault(); 
+      
+   
+      mainContent.style.transform = 'translateY(100vh)';
+      mainContent.style.transition = 'transform 0.5s ease';
+      
+      
+      setTimeout(() => {
+        window.location.href = event.target.getAttribute('href');
+      }, 50000); 
+    });
+  });
+
+ 
+  menuIcon.addEventListener('click', () => {
+    mainContent.style.transform = 'translateY(-100vh)';
+    mainContent.style.transition = 'transform 0.5s ease';
+
+    setTimeout(() => {
+      window.location.href = 'index.html';
+    }, 50000); 
+  });
+
+
+  window.addEventListener('load', () => {
+    mainContent.style.transform = 'translateY(0)';
+  });
+});
